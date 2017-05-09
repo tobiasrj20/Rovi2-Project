e38e9cabefcf6c7d21eb3c75f1d1f6abc8c0ec97
AllanGErnst <allan@productionaid.dk>
2017-05-09 20:41:44 +0200

    det er ikke lavet det store

--- catkin_ws/devel/setup.sh
+++ catkin_ws/devel/setup.sh
@@ -8,7 +8,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/tobias/Dropbox/RobTek/Cand_2_semester/Rovi2-Project/catkin_ws/devel}
+: ${_CATKIN_SETUP_DIR:=/home/age/LAGER/Dropbox/SDU/8_semester/RoVi2/workspace/Rovi2-Project/catkin_ws/devel}
 _SETUP_UTIL="$_CATKIN_SETUP_DIR/_setup_util.py"
 unset _CATKIN_SETUP_DIR
 
