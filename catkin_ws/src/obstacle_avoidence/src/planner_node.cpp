b6f804362fab9fb00320cc81b18be7bd73690424
tobiasrj20 <tobiasrj20@gmail.com>
2017-05-09 17:47:11 +0200

    catkin workspace and node is working

---
+++ catkin_ws/src/obstacle_avoidence/src/planner_node.cpp
@@ -0,0 +1,12 @@
+#include "ros/ros.h"
+#include <iostream>
+#include "../../../../libs/SocketCommunication/SocketCommunication.hpp"
+
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+    cout << "Test" << endl;
+    return 0;
+}
